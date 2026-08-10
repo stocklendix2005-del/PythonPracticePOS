0729e738ce0643b7543920a9c5420f649adf69d0
Lendix <stocklendix2005@gmail.com>
2026-08-10 15:14:30 +0300

    customer Load

--- gadgetsPOS/customers.py
+++ gadgetsPOS/customers.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from tkinter import ttk
 from CustomTitleBar import CustomToplevel
+from database import get_customers
 
 
 def open_customers(root):
@@ -61,3 +63,51 @@ def open_customers(root):
         bg="#0C131A",
     )
     search_button.pack(side="left")
+
+    body_frame = Frame(win.content_area, bg="#06203A")
+    body_frame.pack(fill=BOTH, expand=True)
+
+    scrollbar = Scrollbar(body_frame, orient="vertical")
+    tree = ttk.Treeview(
+        body_frame,
+        columns=(
+            "CUSTOMER CODE",
+            "CUSTOMER NAME",
+            "CUSTOMER I.D",
+            "CUSTOMER LOCATION",
+            "CUSTOMER STATUS",
+        ),
+        show="headings",
+        style="customer.Treeview",
+        yscrollcommand=scrollbar.set,
+    )
+    scrollbar.configure(command=tree.yview)
+
+    tree.column("CUSTOMER CODE", width=120)
+    tree.column("CUSTOMER NAME", width=120)
+    tree.column("CUSTOMER I.D", width=120)
+    tree.column("CUSTOMER LOCATION", width=120)
+    tree.column("CUSTOMER STATUS", width=120)
+
+    tree.heading("CUSTOMER CODE", text="CUSTOMER CODE")
+    tree.heading("CUSTOMER NAME", text="CUSTOMER NAME")
+    tree.heading("CUSTOMER I.D", text="CUSTOMER I.D")
+    tree.heading("CUSTOMER LOCATION", text="CUSTOMER LOCATION")
+    tree.heading("CUSTOMER STATUS", text="CUSTOMER STATUS")
+
+    scrollbar.pack(side="right")
+    tree.pack(fill="both")
+
+    customers = get_customers()
+
+    def load_data(data):
+        for row in tree.get_children():
+            tree.delete(row)
+        for phone in data:
+            tree.insert(
+                "",
+                END,
+                values=(phone[1], phone[3], phone[2], phone[4], phone[5], phone[6]),
+            )
+
+    load_data(customers)
